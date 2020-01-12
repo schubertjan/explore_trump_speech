720d4d5c574820c924ce5c1234c4d0f2f928c917
schubertjan <honza.schubert@gmail.com>
2020-01-12 12:49:06 +0100

    init commit

---
+++ scr/render.R
@@ -0,0 +1,4 @@
+rmarkdown::render(input = "./scr/explore_text.Rmd", 
+                  output_format = "html_document", 
+                  output_dir = "./docs/", 
+                  output_file = "explore_trump_speeches.html")
